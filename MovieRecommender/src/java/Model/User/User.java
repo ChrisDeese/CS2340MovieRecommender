114c0e350c59d8eccc737d442ac7e459270b9e1c
Jacob Watson <jacobwatson.d@gmail.com>
2015-06-10 11:24:48 -0400

    Added Registration

--- MovieRecommender/src/java/Model/User/User.java
+++ MovieRecommender/src/java/Model/User/User.java
@@ -21,36 +21,64 @@ import javax.faces.context.FacesContext;
 public class User implements Serializable{
     private String username;
     private String password;
-    
+    private String email;
+    private boolean admin;
+
     @ManagedProperty("#{userManager}")
     private UserManager userManager;
-    
+
     /**
      * Creates a new instance of User
      */
     public User() {
-        
+
     }
-    
+
+    /**
+     * Returns username of this user
+     * @return username
+     */
     public String getUsername() {
         return this.username;
     }
-    
+
+    /**
+     * Returns password of this user
+     * @return password
+     */
     public String getPassword() {
         return this.password;
     }
-    
+
+    /**
+     * Returns email of this user
+     * @return email
+     */
+    public String getEmail() {
+        return this.email;
+    }
+
+    /**
+     * Sets username of this user
+     */
     public void setUsername(String un) {
         this.username = un;
     }
-    
+
+    /**
+     * Sets password of this user
+     */
     public void setPassword(String p) {
         this.password = p;
     }
-    
+
+    /**
+     * Logs in user. If password is wrong, login fails
+     * @return page redirect
+     */
     public String login() {
         UserData data = userManager.find(username);
-        
+
         if (data == null || !data.checkP(password)) {
             username="";
             password="";
@@ -62,19 +90,55 @@ public class User implements Serializable{
         System.out.println("Login Success");
             return "SuccessPage";
     }
-    
+
+    /**
+     * Logs out user
+     * @return page redirect
+     */
     public String logout() {
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
         return "welcomePage.xhtml?faces-redirect=true";
     }
-    
+
+    /**
+     * Registers user. If username is already taken, registration fails
+     * @return page redirect
+     */
+    public String register() {
+        if (username == null || password == null || email == null) {
+            System.out.println("Input is null.");
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage("Username/Password/Email is null"));
+            return null;
+        } else if (userManager.find(username) != null) {
+            System.out.println("Username already taken, please choose another.");
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage("Username already taken"));
+            return null;
+        }
+
+        UserData data = new UserData(username, password);
+        data.setEmail(email);
+        data.setAdmin(admin);
+        userManager.addUser(data);
+        System.out.println("Registration Success");
+        return "index";
+    }
+
+    /**
+     * Cancels login/registration
+     * @return page redirect
+     */
     public String cancel() {
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
         return "welcomePage.xhtml?faces-redirect=true";
     }
-    
+
+    /**
+     * Sets the user manager for this user, which is used as the database
+     */
     public void setUserManager(UserManager um) {
         userManager = um;
     }
-    
+
 }
--- MovieRecommender/src/java/Model/User/UserData.java
+++ MovieRecommender/src/java/Model/User/UserData.java
@@ -8,23 +8,74 @@ package Model.User;
 /**
  *
  * @author chris
- * 
+ *
  * This class to be deleted once database is implemented.
  */
 public class UserData {
 
-    
+
     private String username;
     private String password;
-    //more to be added later
-    
+    private String email;
+    private boolean admin;
+
+    //To add later
+    /*private String name;
+    private String bio;
+    private String location;
+    private int age;*/
+
+    /**
+     * Creates a new instance of User
+     */
     UserData(String name, String pass) {
         this.username = name;
         this.password = pass;
     }
-    
+
+    /**
+     * Checks entered password versus saved password
+     * @return p = password
+     */
     boolean checkP(String p) {
         return (p.equals(password));
     }
-    
+
+    /**
+     * Sets the email of this user
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Sets whether this user has admin privileges
+     */
+    public void setAdmin(boolean b) {
+        this.admin = b;
+    }
+
+    /**
+     * Gets the username of this user
+     * @return username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /*public String getName() {
+        return name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getAge() {
+        return age;
+    }*/
 }

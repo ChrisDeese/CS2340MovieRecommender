e866b9999f7e3a879ae6054c84419bfd6c3f5d7b
Jacob Watson <jacobwatson.d@gmail.com>
2015-06-19 14:37:43 -0400

    New Movies and DVDs

--- MovieRecommender/src/java/Search.java
+++ MovieRecommender/src/java/Search.java
@@ -49,6 +49,40 @@ public class Search implements Serializable {
         movies = results;
     }
 
+    public void newTheater() {
+        URL jsonRequest = new URL("http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/new_releases.json?apikey=yedukp76ffytfuy24zsqk7f5&page_limit=20");
+        URLConnection connection = jsonRequest.openConnection();
+        connection.setDoOutput(true);
+        Scanner scanner = new Scanner(wikiRequest.openStream());
+        String response = scanner.useDelimiter("\\Z").next();
+        JSONArray json = (JSONArray) Util.parseJson(response);
+        scanner.close();
+
+        List<Movie> results = new ArrayList<Movie>();
+        for (int c = 1; c < json.size(); c++) {
+            results.add(json.get(c));
+        }
+
+        movies = results;
+    }
+
+    public void newDVD() {
+        URL jsonRequest = new URL("http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=yedukp76ffytfuy24zsqk7f5&page_limit=20");
+        URLConnection connection = jsonRequest.openConnection();
+        connection.setDoOutput(true);
+        Scanner scanner = new Scanner(wikiRequest.openStream());
+        String response = scanner.useDelimiter("\\Z").next();
+        JSONArray json = (JSONArray) Util.parseJson(response);
+        scanner.close();
+
+        List<Movie> results = new ArrayList<Movie>();
+        for (int c = 1; c < json.size(); c++) {
+            results.add(json.get(c));
+        }
+
+        movies = results;
+    }
+
     public List<Movie> getMovies() {
         return movies;
     }

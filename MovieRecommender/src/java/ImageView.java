7c96c8e8136297f7dc00ad28d581c40ead693261
Toni <t_tiger104@yahoo.com>
2015-07-07 01:05:09 -0400

    HTML fixes, added admin pages

---
+++ MovieRecommender/src/java/ImageView.java
@@ -0,0 +1,24 @@
+package org.primefaces.showcase.view.multimedia;
+ 
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class ImageView {
+     
+    private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 8; i++) {
+            images.add("Movie"+i+".jpg");
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}

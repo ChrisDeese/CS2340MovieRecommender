7c340b4171f9dad7c89d9a9e9046f966f8787787
tjohnson95 <t_tiger104@yahoo.com>
2015-07-09 16:15:07 -0400

    Update Movie.java

--- MovieRecommender/src/java/Movie.java
+++ MovieRecommender/src/java/Movie.java
@@ -1,5 +1,4 @@
 
-import Model.User.UserData;
 import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -12,49 +11,51 @@ import org.json.simple.JSONObject;
  * @author jacob
  */
 public class Movie {
+
     private JSONObject json;
     private String title;
-    private String Id;
+    private String iD;
     private int rating;
     private int movieId;
     private int ratingTotal;
     private int ratingCount;
     private String major;
-    
+
     private static SessionFactory factory2;
 
     //TODO
     //Cast
     //Reviews
     //Clips
-
     public Movie() {
-        
+
     }
-    
+
     /**
      * Creates a new instance of a Movie
+     *
      * @param json
      */
     public Movie(JSONObject json) {
         this.json = json;
         this.title = (String) json.get("title");
-        this.Id = (String) json.get("id");
+        this.iD = (String) json.get("id");
     }
-    
+
     public void rateMovie(int i) {
         this.setRatingCount(this.getRatingCount() + 1);
         this.setRatingTotal(this.getRatingTotal() + i);
         this.setRating(this.getRatingTotal() / this.getRatingCount());
         try {
             factory2 = new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) { 
+        } catch (Throwable ex) {
             System.err.println("Failed to create sessionFactory object." + ex);
-            throw new ExceptionInInitializerError(ex); 
+            throw new ExceptionInInitializerError(ex);
         }
         System.out.println(this.movieId);
         Session session = factory2.openSession();
-        Query query = session.createQuery("from Movie where id = '" + this.Id + "'");
+        Query query = session.createQuery(
+                "from Movie where id = '" + this.iD + "'");
         List<Movie> movs = query.list();
         System.out.println(movs.size());
         Transaction tx = null;
@@ -68,80 +69,87 @@ public class Movie {
 
     /**
      * Get total from all ratings
-     * @return 
+     *
+     * @return
      */
     public int getRatingTotal() {
         return this.ratingTotal;
     }
-    
+
     public String getMajor() {
-        
+
         return this.major;
     }
-    
+
     public void setMajor(String s) {
         this.major = s;
     }
-    
+
     /**
      * Set total of ratings
-     * @param num 
+     *
+     * @param num
      */
     public void setRatingTotal(int num) {
         this.ratingTotal = num;
     }
-    
+
     /**
      * Get number of times movie has been rated
-     * @return 
+     *
+     * @return
      */
     public int getRatingCount() {
         return this.ratingCount;
     }
-    
+
     public void setTitle(String s) {
         this.title = s;
     }
-    
+
     /**
      * Set the number of times this movie has been rated
-     * @param num 
+     *
+     * @param num
      */
     public void setRatingCount(int num) {
         this.ratingCount = num;
     }
-    
-    
+
     /**
      * Get database Id for the movie
-     * @return 
+     *
+     * @return
      */
     public int getMovieId() {
         return this.movieId;
     }
-    
+
     /**
      * Set the database id for the movie (not used)
-     * @param Id 
+     *
+     * @param Id
      */
-    public void setMovieId(int Id) {
-        this.movieId = Id;
+    public void setMovieId(int id) {
+        this.movieId = id;
     }
-    
+
     /**
      * Gets the rotten tomatoes ID of this movie
+     *
      * @return id
      */
     public String getId() {
-        return this.Id;
+        return this.iD;
     }
 
     public void setId(String id) {
-        this.Id = id;
+        this.iD = id;
     }
-    
+
     /**
      * Gets the title of this movie
+     *
      * @return title
      */
     public String getTitle() {
@@ -150,6 +158,7 @@ public class Movie {
 
     /**
      * Gets the MPAA rating of this movie
+     *
      * @return mpaa_rating
      */
     public String getMPAA() {
@@ -158,6 +167,7 @@ public class Movie {
 
     /**
      * Gets the theater release date of this movie
+     *
      * @return release_date_theater
      */
     public String getReleaseT() {
@@ -166,6 +176,7 @@ public class Movie {
 
     /**
      * Gets the dvd release date of this movie
+     *
      * @return release_date_dvd
      */
     public String getReleaseD() {
@@ -174,6 +185,7 @@ public class Movie {
 
     /**
      * Gets the synopsis of this movie
+     *
      * @return synopsis
      */
     public String getSynopsis() {
@@ -183,6 +195,7 @@ public class Movie {
 
     /**
      * Gets the director of this movie
+     *
      * @return director
      */
     public String getDirector() {
@@ -191,6 +204,7 @@ public class Movie {
 
     /**
      * Gets the studio of this movie
+     *
      * @return studio
      */
     public String getStudio() {
@@ -199,6 +213,7 @@ public class Movie {
 
     /**
      * Gets the runtime of this movie
+     *
      * @return runtime
      */
     public long getRuntime() {
@@ -207,6 +222,7 @@ public class Movie {
 
     /**
      * Gets the critic's consensus of this movie
+     *
      * @return critics_consensus
      */
     public String getConsensus() {
@@ -215,6 +231,7 @@ public class Movie {
 
     /**
      * Gets the critic's rating of this movie
+     *
      * @return critics_rating
      */
     public String getCRating() {
@@ -223,6 +240,7 @@ public class Movie {
 
     /**
      * Gets the audience's rating of this movie
+     *
      * @return audience_rating
      */
     public String getARating() {
@@ -231,6 +249,7 @@ public class Movie {
 
     /**
      * Gets the critic's score of this movie
+     *
      * @return critics_score
      */
     public int getCScore() {
@@ -239,6 +258,7 @@ public class Movie {
 
     /**
      * Gets the audience's score of this movie
+     *
      * @return audience_score
      */
     public String getAScore() {
@@ -247,6 +267,7 @@ public class Movie {
 
     /**
      * Gets the thumbnail of the movie's poster
+     *
      * @return thumbnail
      */
     public String getThumbnail() {
@@ -256,37 +277,37 @@ public class Movie {
 
     //TODO
     //Figure out what these are
-
     /**
      * Gets the profile of the movie's poster
+     *
      * @return profile
      */
     //public String getProfile() {
     //    return profile;
     //}
-
     /**
      * Gets the thumbnail of the movie's poster
+     *
      * @return thumbnail
      */
     //public String getDetailed() {
     //    return detailed;
     //}
-
     /**
      * Gets the original image of the movie's poster
+     *
      * @return original
      */
     public String getOriginal() {
         return (String) json.get("original");
     }
-    
+
     public int getRating() {
         return this.rating;
     }
-    
+
     public void setRating(int rating) {
-        this.rating=rating;
+        this.rating = rating;
     }
-    
+
 }

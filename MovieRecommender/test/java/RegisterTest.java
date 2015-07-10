56f196b06c5030b09a89eca296af1f00304fe37f
Toni <t_tiger104@yahoo.com>
2015-07-10 18:41:42 -0400

    added junit test

---
+++ MovieRecommender/test/java/RegisterTest.java
@@ -0,0 +1,98 @@
+package java;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import model.user.User;
+import model.user.UserData;
+import model.user.UserManager;
+import org.junit.Assert;
+
+/**
+ *
+ * @author Toni Johnson
+ */
+public class RegisterTest {
+
+    private UserData uData;
+    private User user;
+
+    public RegisterTest() {
+    }
+
+    @Before
+    public void setUp() {
+        user = new User();
+
+    }
+
+    @Test
+    //Test normal registration
+    public void testNewUser() {
+        user.setUsername("test1");
+        user.setPassword("pass1");
+        user.setName("Junit");
+
+        assertEquals("Should print register success!", user.register());
+    }
+
+    //test of registration with empty username
+    @Test
+    public void testNoUsername() {
+        user.setUsername("");
+        user.setPassword("pass1");
+        user.setName("Junit");
+        assertEquals("Should return 'username field empty' and not register",
+                user.register());
+    }
+
+    //test with no password
+    @Test
+    public void testNoPassword() {
+        user.setUsername("test2");
+        user.setPassword("");
+        user.setName("Junit");
+        assertEquals("Should return password empty and not register",
+                user.register());
+
+    }
+
+    //test with both username and password empty
+    @Test
+    public void testBothfieldsEmpty() {
+        user.setUsername("");
+        user.setPassword("");
+        user.setName("Junit");
+        assertEquals("Should return password and username empty and "
+                + "not register",
+                user.register());
+
+    }
+
+    //test trying to register an already taken username
+
+    @Test
+    public void testAlreadyUsedUname() {
+        user.setUsername("test3");
+        user.setPassword("pass3");
+        user.setName("Junit");
+        user.register();
+        User user1 = new User();
+        user1.setUsername("test3");
+        user1.setPassword("pass3");
+        user1.setName("Junit");
+        assertEquals("Should return username already taken and not register. ",
+                user1.register());
+
+    }
+
+}
